932d67feeef470a62559df3dfc573141dcd0e38e
Samuel Satizabal <samuelsatizabal@hotmail.com>
2020-10-08 01:57:32 -0500

    First Commit, program now showing data from the dataset and filtering successfully :D

---
+++ HeartDiseaseInvestigation/HeartDiseaseInvestigation/UI/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace HeartDiseaseInvestigation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.heartDataTable = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxHeartData = new System.Windows.Forms.ComboBox();
+            this.filter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.heartDataTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // heartDataTable
+            // 
+            this.heartDataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.heartDataTable.Location = new System.Drawing.Point(12, 141);
+            this.heartDataTable.Name = "heartDataTable";
+            this.heartDataTable.RowHeadersWidth = 51;
+            this.heartDataTable.RowTemplate.Height = 24;
+            this.heartDataTable.Size = new System.Drawing.Size(1108, 541);
+            this.heartDataTable.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(175, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(291, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Put the same value for a search of one value";
+            // 
+            // comboBoxHeartData
+            // 
+            this.comboBoxHeartData.FormattingEnabled = true;
+            this.comboBoxHeartData.Location = new System.Drawing.Point(12, 26);
+            this.comboBoxHeartData.Name = "comboBoxHeartData";
+            this.comboBoxHeartData.Size = new System.Drawing.Size(284, 24);
+            this.comboBoxHeartData.TabIndex = 5;
+            // 
+            // filter
+            // 
+            this.filter.Location = new System.Drawing.Point(309, 51);
+            this.filter.Name = "filter";
+            this.filter.Size = new System.Drawing.Size(90, 32);
+            this.filter.TabIndex = 6;
+            this.filter.Text = "Filter data";
+            this.filter.UseVisualStyleBackColor = true;
+            this.filter.Click += new System.EventHandler(this.filter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 694);
+            this.Controls.Add(this.filter);
+            this.Controls.Add(this.comboBoxHeartData);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.heartDataTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.heartDataTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView heartDataTable;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxHeartData;
+        private System.Windows.Forms.Button filter;
+    }
+}
+
---
+++ HeartDiseaseInvestigation/HeartDiseaseInvestigation/UI/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeartDiseaseInvestigation
+{
+    
+    public partial class Form1 : Form
+    {
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            loadDt();
+        }
+
+        //this method load the csv and load this data into a dataTable
+        private void loadDt()
+        {
+            dt.Clear();
+            string dir = "../../Data/heart.csv";
+            if (File.Exists(dir))
+            {
+
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(dir);
+                string[] columns = lines[0].Split(',');
+
+                for (int i = 0; i < columns.Length; i++)
+                {
+                    dt.Columns.Add(columns[i]);
+                    comboBoxHeartData.Items.Add(columns[i]);
+                }
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    //split the line by ","
+                    string[] aux = lines[i].Split(',');
+                    dt.Rows.Add(aux);
+                }
+            }
+            heartDataTable.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void filter_Click(object sender, EventArgs e)
+        {
+            dt.DefaultView.RowFilter = comboBoxHeartData.Text + " >= " + "'" + textBox1.Text + "'" + " AND " + comboBoxHeartData.Text + " <= " + "'" + textBox2.Text + "'";
+        }
+    }
+}
