8ef8ff2565d84172845d609d24e2a349981935b0
Juan Ossa <juan.ossa@hotmail.com>
2020-11-06 20:42:08 -0500

    working on server connection

---
+++ TestingConectionToServerPython/TestingConectionToServerPython/Main/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingConectionToServerPython.Model;
+
+namespace TestingConectionToServerPython
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Conecction conect = new Conecction();
+            Console.WriteLine(conect.GetReleases()) ;
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ TestingConectionToServerPython/TestingConectionToServerPython/Model/Conecction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingConectionToServerPython.Model
+{
+    public class Conecction
+    {
+        public Conecction() {
+            
+        }
+        public string GetReleases() {
+            return "test";
+        }
+    }
+}
