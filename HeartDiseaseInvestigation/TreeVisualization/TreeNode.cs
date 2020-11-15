8281c7a3414e792b0d453737d5a4a1072873b099
Zonitidus <zonitidus@gmail.com>
2020-11-15 14:01:07 -0500

    Added horizontal tree arrangement.

--- HeartDiseaseInvestigation/TreeVisualization/TreeNode.cs
+++ HeartDiseaseInvestigation/TreeVisualization/TreeNode.cs
@@ -53,6 +53,86 @@ namespace HeartDiseaseInvestigation.TreeVisualization
             this.font = font;
         }
 
-        
+        public void AddTrueNode(TreeNode node)
+        {
+            this.TrueNode = node;
+        }
+
+        public void AddFalseNode(TreeNode node)
+        {
+            this.FalseNode = node;
+        }
+
+        public void Arrange(Graphics gr, ref float xmin, ref float ymin)
+        {
+            this.ArrangeHorizontally(gr, ref xmin, ref ymin);
+        }
+
+        private void ArrangeHorizontally(Graphics graph, ref float xmin, ref float ymin)
+        {
+            SizeF size = data.GetSize(graph, this.font);
+
+            float x = xmin;
+            float biggest_ymin = ymin + size.Height;
+            float subtree_ymin = ymin + size.Height + VOffset;
+
+
+
+            if(this.TrueNode != null)
+            {
+                float child_ymin = subtree_ymin;
+                this.TrueNode.Arrange(graph, ref x, ref child_ymin);
+
+                if (biggest_ymin < child_ymin) biggest_ymin = child_ymin;
+
+                x += HOffset;
+            }
+
+            if(this.FalseNode != null)
+            {
+                float child_ymin = subtree_ymin;
+                this.FalseNode.Arrange(graph, ref x, ref child_ymin);
+
+                if (biggest_ymin < child_ymin) 
+                    biggest_ymin = child_ymin;
+
+                x += HOffset;
+            }
+
+            if (this.TrueNode != null || this.FalseNode != null) 
+                x -= HOffset;
+
+
+            float subtree_width = x - xmin;
+            if (size.Width > subtree_width)
+            {
+                x = xmin + (size.Width - subtree_width) / 2;
+                
+                if(this.TrueNode != null)
+                {
+                    this.TrueNode.Arrange(graph, ref x, ref subtree_ymin);
+                    x += HOffset;
+                }
+
+                if (this.FalseNode != null)
+                {
+                    this.FalseNode.Arrange(graph, ref x, ref subtree_ymin);
+                    x += HOffset;
+                }
+
+                subtree_width = size.Width;
+            }
+
+            DataCenter = new PointF(
+                xmin + subtree_width / 2,
+                ymin + size.Height / 2);
+
+
+            xmin += subtree_width;
+
+ 
+            ymin = biggest_ymin;
+        }
+
     }
 }
