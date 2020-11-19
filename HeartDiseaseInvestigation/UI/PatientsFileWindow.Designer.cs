b9cf3f9c388b799bbb12837044c8576b36070b97
Samuel Satizabal <samuelsatizabal@hotmail.com>
2020-11-18 21:39:25 -0500

    added zoom control in the image view

--- HeartDiseaseInvestigation/UI/PatientsFileWindow.Designer.cs
+++ HeartDiseaseInvestigation/UI/PatientsFileWindow.Designer.cs
@@ -30,18 +30,23 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.dataFilterControl1 = new HeartDiseaseInvestigation.UI.DataFilterControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.graphsControl1 = new HeartDiseaseInvestigation.UI.GraphsControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.buttonResetZoom = new System.Windows.Forms.Button();
+            this.zoomOut = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBoxDecisionTree = new System.Windows.Forms.PictureBox();
+            this.pictureBoxTree = new System.Windows.Forms.PictureBox();
+            this.zoomIn = new System.Windows.Forms.Button();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.tabPage3.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDecisionTree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTree)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -49,6 +54,8 @@
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -66,6 +73,13 @@
             this.tabPage1.Text = "Data";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // dataFilterControl1
+            // 
+            this.dataFilterControl1.Location = new System.Drawing.Point(1, 0);
+            this.dataFilterControl1.Name = "dataFilterControl1";
+            this.dataFilterControl1.Size = new System.Drawing.Size(1119, 649);
+            this.dataFilterControl1.TabIndex = 0;
+            // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.graphsControl1);
@@ -77,23 +91,6 @@
             this.tabPage2.Text = "Graphs";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // tabPage3
-            // 
-            this.tabPage3.Controls.Add(this.panel1);
-            this.tabPage3.Location = new System.Drawing.Point(4, 25);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1126, 658);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Decision Tree";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // dataFilterControl1
-            // 
-            this.dataFilterControl1.Location = new System.Drawing.Point(1, 0);
-            this.dataFilterControl1.Name = "dataFilterControl1";
-            this.dataFilterControl1.Size = new System.Drawing.Size(1119, 649);
-            this.dataFilterControl1.TabIndex = 0;
-            // 
             // graphsControl1
             // 
             this.graphsControl1.Location = new System.Drawing.Point(22, 38);
@@ -101,23 +98,83 @@
             this.graphsControl1.Size = new System.Drawing.Size(1123, 655);
             this.graphsControl1.TabIndex = 3;
             // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.buttonResetZoom);
+            this.tabPage3.Controls.Add(this.zoomOut);
+            this.tabPage3.Controls.Add(this.panel1);
+            this.tabPage3.Controls.Add(this.zoomIn);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1126, 658);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Decision Tree Auto";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // buttonResetZoom
+            // 
+            this.buttonResetZoom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonResetZoom.Location = new System.Drawing.Point(125, 3);
+            this.buttonResetZoom.Name = "buttonResetZoom";
+            this.buttonResetZoom.Size = new System.Drawing.Size(145, 49);
+            this.buttonResetZoom.TabIndex = 3;
+            this.buttonResetZoom.Text = "Reset Zoom";
+            this.buttonResetZoom.UseVisualStyleBackColor = true;
+            this.buttonResetZoom.Click += new System.EventHandler(this.buttonResetZoom_Click);
+            // 
+            // zoomOut
+            // 
+            this.zoomOut.Location = new System.Drawing.Point(67, 3);
+            this.zoomOut.Name = "zoomOut";
+            this.zoomOut.Size = new System.Drawing.Size(52, 49);
+            this.zoomOut.TabIndex = 2;
+            this.zoomOut.UseVisualStyleBackColor = true;
+            this.zoomOut.Click += new System.EventHandler(this.zoomOut_Click);
+            // 
             // panel1
             // 
             this.panel1.AutoScroll = true;
-            this.panel1.Controls.Add(this.pictureBoxDecisionTree);
-            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Controls.Add(this.pictureBoxTree);
+            this.panel1.Location = new System.Drawing.Point(3, 58);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1120, 652);
+            this.panel1.Size = new System.Drawing.Size(1123, 597);
             this.panel1.TabIndex = 0;
             // 
-            // pictureBoxDecisionTree
+            // pictureBoxTree
             // 
-            this.pictureBoxDecisionTree.Location = new System.Drawing.Point(3, 3);
-            this.pictureBoxDecisionTree.Name = "pictureBoxDecisionTree";
-            this.pictureBoxDecisionTree.Size = new System.Drawing.Size(1114, 646);
-            this.pictureBoxDecisionTree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBoxDecisionTree.TabIndex = 1;
-            this.pictureBoxDecisionTree.TabStop = false;
+            this.pictureBoxTree.Location = new System.Drawing.Point(4, 3);
+            this.pictureBoxTree.Name = "pictureBoxTree";
+            this.pictureBoxTree.Size = new System.Drawing.Size(1116, 594);
+            this.pictureBoxTree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxTree.TabIndex = 0;
+            this.pictureBoxTree.TabStop = false;
+            // 
+            // zoomIn
+            // 
+            this.zoomIn.Location = new System.Drawing.Point(7, 3);
+            this.zoomIn.Name = "zoomIn";
+            this.zoomIn.Size = new System.Drawing.Size(54, 49);
+            this.zoomIn.TabIndex = 1;
+            this.zoomIn.UseVisualStyleBackColor = true;
+            this.zoomIn.Click += new System.EventHandler(this.zoomIn_Click);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(1126, 658);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Decision Tree Manual";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 25);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(1126, 658);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Add Patient";
+            this.tabPage5.UseVisualStyleBackColor = true;
             // 
             // PatientsFileWindow
             // 
@@ -132,8 +189,7 @@
             this.tabPage2.ResumeLayout(false);
             this.tabPage3.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDecisionTree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTree)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -147,7 +203,12 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBoxDecisionTree;
+        private System.Windows.Forms.PictureBox pictureBoxTree;
+        private System.Windows.Forms.Button zoomIn;
+        private System.Windows.Forms.Button zoomOut;
+        private System.Windows.Forms.Button buttonResetZoom;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
     }
 }
 
--- HeartDiseaseInvestigation/UI/PatientsFileWindow.cs
+++ HeartDiseaseInvestigation/UI/PatientsFileWindow.cs
@@ -16,13 +16,37 @@ namespace HeartDiseaseInvestigation
 
     public partial class PatientsFileWindow : Form
     {
+        int w;
+        int h;
         public PatientsFileWindow()
         {
             InitializeComponent();
-            pictureBoxDecisionTree.Image = Image.FromFile("../../data/img.png");
-            //pictureBoxDecisionTree.SizeMode = PictureBoxSizeMode.StretchImage;
-            //pictureBoxDecisionTree.
+            pictureBoxTree.Image = Image.FromFile("../../Data/img.png");
+            w = pictureBoxTree.Width;
+            h = pictureBoxTree.Height;
+            zoomIn.BackgroundImage = Image.FromFile("../../Data/zoomIn.png");
+            zoomOut.BackgroundImage = Image.FromFile("../../Data/zoomOut.png");
+            zoomIn.BackgroundImageLayout = ImageLayout.Stretch;
+            zoomOut.BackgroundImageLayout = ImageLayout.Stretch;
+            this.Icon = new Icon("../../Data/logo.ico");
         }
 
+        private void zoomIn_Click(object sender, EventArgs e)
+        {
+            pictureBoxTree.Width += 20;
+            pictureBoxTree.Height += 20;
+        }
+
+        private void zoomOut_Click(object sender, EventArgs e)
+        {
+            pictureBoxTree.Width -= 20;
+            pictureBoxTree.Height -= 20;
+        }
+
+        private void buttonResetZoom_Click(object sender, EventArgs e)
+        {
+            pictureBoxTree.Width = w;
+            pictureBoxTree.Height = h;
+        }
     }
 }
