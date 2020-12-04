1ce7451320ae4360425272a92c246d79c1633e35
Samuel Satizabal <samuelsatizabal@hotmail.com>
2020-12-04 11:36:36 -0500

    added the names of the developers

--- HeartDiseaseInvestigation/UI/PatientsFileWindow.Designer.cs
+++ HeartDiseaseInvestigation/UI/PatientsFileWindow.Designer.cs
@@ -30,7 +30,9 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataFilterControl1 = new HeartDiseaseInvestigation.UI.DataFilterControl();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.graphsControl1 = new HeartDiseaseInvestigation.UI.GraphsControl();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.labelTrainTreeAuto = new System.Windows.Forms.Label();
             this.buttonTrainTreeAuto = new System.Windows.Forms.Button();
@@ -40,15 +42,27 @@
             this.pictureBoxTree = new System.Windows.Forms.PictureBox();
             this.zoomIn = new System.Windows.Forms.Button();
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.accu = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBoxTreeManual = new System.Windows.Forms.PictureBox();
             this.tabPage5 = new System.Windows.Forms.TabPage();
             this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
-            this.dataFilterControl1 = new HeartDiseaseInvestigation.UI.DataFilterControl();
             this.addPatientControl2 = new HeartDiseaseInvestigation.UI.AddPatientControl();
-            this.graphsControl1 = new HeartDiseaseInvestigation.UI.GraphsControl();
             this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -60,6 +74,11 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTreeManual)).BeginInit();
             this.tabPage5.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.tabPage6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -76,7 +95,7 @@
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1464, 855);
+            this.tabControl1.Size = new System.Drawing.Size(1764, 855);
             this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
             this.tabControl1.TabIndex = 4;
             this.tabControl1.Click += new System.EventHandler(this.tabControl1_Click);
@@ -92,6 +111,18 @@
             this.tabPage1.Text = "Data";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // dataFilterControl1
+            // 
+            this.dataFilterControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataFilterControl1.AutoScroll = true;
+            this.dataFilterControl1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.dataFilterControl1.Location = new System.Drawing.Point(162, 3);
+            this.dataFilterControl1.Name = "dataFilterControl1";
+            this.dataFilterControl1.Size = new System.Drawing.Size(1458, 847);
+            this.dataFilterControl1.TabIndex = 0;
+            // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.graphsControl1);
@@ -102,6 +133,14 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Graphs";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // graphsControl1
+            // 
+            this.graphsControl1.Location = new System.Drawing.Point(186, 171);
+            this.graphsControl1.Name = "graphsControl1";
+            this.graphsControl1.Size = new System.Drawing.Size(1637, 514);
+            this.graphsControl1.TabIndex = 3;
+            this.graphsControl1.Load += new System.EventHandler(this.graphsControl1_Load);
             // 
             // tabPage3
             // 
@@ -195,15 +234,6 @@
             this.tabPage4.Text = "Decision Tree Manual";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // panel2
-            // 
-            this.panel2.AutoScroll = true;
-            this.panel2.Controls.Add(this.pictureBoxTreeManual);
-            this.panel2.Location = new System.Drawing.Point(3, 77);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1900, 865);
-            this.panel2.TabIndex = 0;
-            // 
             // accu
             // 
             this.accu.AutoSize = true;
@@ -212,6 +242,15 @@
             this.accu.Name = "accu";
             this.accu.Size = new System.Drawing.Size(0, 44);
             this.accu.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.Controls.Add(this.pictureBoxTreeManual);
+            this.panel2.Location = new System.Drawing.Point(3, 77);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1900, 865);
+            this.panel2.TabIndex = 0;
             // 
             // pictureBoxTreeManual
             // 
@@ -244,18 +283,6 @@
             this.pictureBoxLogo.TabIndex = 82;
             this.pictureBoxLogo.TabStop = false;
             // 
-            // dataFilterControl1
-            // 
-            this.dataFilterControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataFilterControl1.AutoScroll = true;
-            this.dataFilterControl1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.dataFilterControl1.Location = new System.Drawing.Point(162, 3);
-            this.dataFilterControl1.Name = "dataFilterControl1";
-            this.dataFilterControl1.Size = new System.Drawing.Size(1458, 847);
-            this.dataFilterControl1.TabIndex = 0;
-            // 
             // addPatientControl2
             // 
             this.addPatientControl2.Location = new System.Drawing.Point(3, 3);
@@ -263,28 +290,168 @@
             this.addPatientControl2.Size = new System.Drawing.Size(1900, 950);
             this.addPatientControl2.TabIndex = 0;
             // 
-            // graphsControl1
-            // 
-            this.graphsControl1.Location = new System.Drawing.Point(186, 171);
-            this.graphsControl1.Name = "graphsControl1";
-            this.graphsControl1.Size = new System.Drawing.Size(1637, 514);
-            this.graphsControl1.TabIndex = 3;
-            this.graphsControl1.Load += new System.EventHandler(this.graphsControl1_Load);
-            // 
             // tabPage6
             // 
+            this.tabPage6.Controls.Add(this.label6);
+            this.tabPage6.Controls.Add(this.label5);
+            this.tabPage6.Controls.Add(this.linkLabel4);
+            this.tabPage6.Controls.Add(this.linkLabel3);
+            this.tabPage6.Controls.Add(this.linkLabel2);
+            this.tabPage6.Controls.Add(this.linkLabel1);
+            this.tabPage6.Controls.Add(this.label4);
+            this.tabPage6.Controls.Add(this.label3);
+            this.tabPage6.Controls.Add(this.label2);
+            this.tabPage6.Controls.Add(this.label1);
+            this.tabPage6.Controls.Add(this.pictureBox4);
+            this.tabPage6.Controls.Add(this.pictureBox3);
+            this.tabPage6.Controls.Add(this.pictureBox2);
+            this.tabPage6.Controls.Add(this.pictureBox1);
             this.tabPage6.Location = new System.Drawing.Point(4, 25);
             this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Size = new System.Drawing.Size(1456, 826);
+            this.tabPage6.Size = new System.Drawing.Size(1756, 826);
             this.tabPage6.TabIndex = 5;
             this.tabPage6.Text = "Developers";
             this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(358, 127);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 144);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(358, 418);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(140, 144);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(1009, 127);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(140, 144);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(1009, 418);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(140, 144);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(504, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 47);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Juan David Ossa";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(504, 434);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(396, 45);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Esteban Yusunguaira";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1155, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(326, 47);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Felipe Garcia";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Arial", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1155, 434);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(326, 45);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Samuel Satizabal";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkLabel1.Location = new System.Drawing.Point(506, 190);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(216, 32);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Perfil en GitHub";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkLabel2.Location = new System.Drawing.Point(506, 479);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(216, 32);
+            this.linkLabel2.TabIndex = 9;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Perfil en GitHub";
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel3.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkLabel3.Location = new System.Drawing.Point(1155, 190);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(216, 32);
+            this.linkLabel3.TabIndex = 10;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "Perfil en GitHub";
+            // 
+            // linkLabel4
+            // 
+            this.linkLabel4.AutoSize = true;
+            this.linkLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel4.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkLabel4.Location = new System.Drawing.Point(1155, 479);
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.Size = new System.Drawing.Size(216, 32);
+            this.linkLabel4.TabIndex = 11;
+            this.linkLabel4.TabStop = true;
+            this.linkLabel4.Text = "Perfil en GitHub";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(731, 791);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(339, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "El Marcapasos De Marta™ is a registered trademark.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(800, 808);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(168, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "All Rights Reserved 2020";
             // 
             // PatientsFileWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1488, 872);
+            this.ClientSize = new System.Drawing.Size(1788, 872);
             this.Controls.Add(this.tabControl1);
             this.Name = "PatientsFileWindow";
             this.Text = "Heart Disease Classifier";
@@ -300,6 +467,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTreeManual)).EndInit();
             this.tabPage5.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.tabPage6.ResumeLayout(false);
+            this.tabPage6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -328,6 +501,20 @@
         private System.Windows.Forms.Label accu;
         private UI.GraphsControl graphsControl1;
         private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.LinkLabel linkLabel4;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
